f9a112e07ee432d5f34b3a167902e808cfb0e84f
Max Strange <maxfieldstrange@gmail.com>
2018-12-21 20:16:08 -0800

    Bump version

--- rebuild.sh
+++ rebuild.sh
@@ -2,7 +2,7 @@ rm -rf build/
 rm -rf dist/
 rm -rf retrowrapper.egg-info/
 pipreqs --force .
-python setup.py bdist_wheel
+python3 setup.py bdist_wheel
 twine upload dist/*
 echo "Done. Please remember to make a release on github via:"
 echo "git tag -a v<VERSION_NUMBER> -m \"<Message>\""
